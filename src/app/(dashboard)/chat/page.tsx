f6649044e53a71cfca3ca8de968f6580efac1902
Mustafa Bhikhapurwala <mustafabhikhpurwala@gmail.com>
2024-09-26 10:05:16 +0000

    changed some chat settings

--- src/app/(dashboard)/chat/page.tsx
+++ src/app/(dashboard)/chat/page.tsx
@@ -4,14 +4,14 @@ import "@copilotkit/react-ui/styles.css";
 export default function Page() {
   return (
     <div className="relative h-full w-full bg-neutral-900">
-      <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
+      <div className="absolute inset-0 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
       <div className="relative">
         <CopilotChat
           labels={{
-            title: "Studdy Buddy 📚",
-            initial: "Welcome to Studdy Buddy! How can I help you today?",
+            title: "Cooper Word",
+            initial: "Welcome to Cooper Word! How can I help you today?",
           }}
-          instructions="When a user asks a question, analyze it to understand the subject and provide a clear, accurate answer relevant to the study topic. Make sure to focus solely on educational content and avoid giving personal advice or unrelated information."
+          instructions="When a user asks a question, analyze it to understand the subject and provide a clear, accurate answer relevant to the creation of documents in form of powerpoint presentation , word , excel , text etc. Make sure to focus solely on educational content and avoid giving personal advice or unrelated information."
           className="min-h-screen p-8 pb-32"
         />
       </div>
