fb2ff52a09c36f022fea5ac57fd16fe776aba6f9
Mustafa <mustafabhikhpurwala@gmail.com>
2024-09-29 12:20:12 +0530

    fix: build fixes and eslint fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import BackgroundParticles from "@/components/BackgroundParticles";
 import Divider from "@/components/Divider";
 import Features from "@/components/Features";
@@ -8,24 +7,6 @@ import Navbar from "@/components/Navbar";
 import { PricingCard } from "@/components/PricingCard";
 
 // PWA 
- 
-import { useState, useEffect } from 'react'
-import { subscribeUser, unsubscribeUser, sendNotification } from './actions'
- 
-function urlBase64ToUint8Array(base64String: string) {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding)
-    .replace(/\\-/g, '+')
-    .replace(/_/g, '/')
- 
-  const rawData = window.atob(base64)
-  const outputArray = new Uint8Array(rawData.length)
- 
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i)
-  }
-  return outputArray
-}
 
 export default function Home() {
   return (
